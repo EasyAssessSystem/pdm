4b499873fdc04ab311d0283995170a036217eb87
Li Cheng <chengli@thoughtworks.com>
2016-05-28 11:21:38 +0800

    fix wrong generic type

--- src/main/java/com/stardust/easyassess/pdm/controllers/RoleController.java
+++ src/main/java/com/stardust/easyassess/pdm/controllers/RoleController.java
@@ -1,5 +1,6 @@
 package com.stardust.easyassess.pdm.controllers;
 
+import com.stardust.easyassess.pdm.models.Role;
 import com.stardust.easyassess.pdm.models.User;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping({"{domain}/data/role"})
 @EnableAutoConfiguration
-public class RoleController extends AbstractMaintenanceController<User>  {
+public class RoleController extends AbstractMaintenanceController<Role>  {
 
     @Override
     protected String getModelName() {
