f977ae3c2f66ddcc370d6cc19521dba4a07f1f53
Li Cheng <chengli@thoughtworks.com>
2016-07-10 21:34:58 +0800

    code group sub list

--- src/main/java/com/stardust/easyassess/pdm/controllers/AssayCodeController.java
+++ src/main/java/com/stardust/easyassess/pdm/controllers/AssayCodeController.java
@@ -1,10 +1,16 @@
 package com.stardust.easyassess.pdm.controllers;
 
+import com.stardust.easyassess.core.query.Selection;
+import com.stardust.easyassess.pdm.common.ListSelectionBuilder;
+import com.stardust.easyassess.pdm.common.ViewContext;
 import com.stardust.easyassess.pdm.models.AssayCode;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin("*")
 @RestController
@@ -15,5 +21,16 @@ public class AssayCodeController extends AbstractMaintenanceController<AssayCode
     @Override
     protected String getModelName() {
         return "assayCode";
+    }
+
+    @Override
+    protected void initSelectionBuilders() {
+        listSelectionBuilders.put("categorized", new ListSelectionBuilder() {
+            @Override
+            public List<Selection> buildSelections(List<Selection> selections, ViewContext context) {
+                selections.add(new Selection("group.id", Selection.Operator.EQUAL, context.getLong("group_id")));
+                return selections;
+            }
+        });
     }
 }
