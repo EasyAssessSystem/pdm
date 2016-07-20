612dd3c40f1ee9b0c0b3470a0cab1f59e77197fc
Li Cheng <chengli@thoughtworks.com>
2016-07-20 21:33:26 +0800

    refine deploy script

--- buildtask/deploy.sh
+++ buildtask/deploy.sh
@@ -3,7 +3,6 @@
 service_name=easyassess-pdm
 host_address=192.168.0.21
 host_port=8180
-operation_port=8180
 uid=root
 jar_home=/root/.jenkins/workspace/PDM/
 api_service_path=/usr/esapp/api-services
@@ -16,13 +15,13 @@ function package()
 
 function startup()
 {
-    echo "shutting down http://$host_address:$operation_port"
-    curl -X POST http://$host_address:$operation_port/shutdown
-    ssh $uid@$host_address java -jar $api_service_path/easyassess-pdm-0.0.1.jar &
+    ssh $uid@$host_address service pdm start
 }
 
 function deploy()
 {
+    echo "shutting down http://$host_address:$host_port"
+    ssh $uid@$host_address service pdm stop
     echo "deploying package to server"
     scp $jar_home/target/easyassess-pdm-0.0.1.jar $uid@$host_address:$api_service_path
 }
