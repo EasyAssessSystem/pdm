3bff51752263e52556dacf5825abd03717bccb6a
Li Cheng <chengli@thoughtworks.com>
2016-07-22 17:51:43 +0800

    using env variables for production config

--- buildtask/deploy.sh
+++ buildtask/deploy.sh
@@ -2,7 +2,6 @@
 
 service_name=easyassess-pdm
 host_address=192.168.0.21
-host_port=8180
 uid=root
 jar_home=/root/.jenkins/workspace/PDM/
 api_service_path=/usr/esapp/api-services
@@ -15,12 +14,12 @@ function package()
 
 function startup()
 {
-    ssh $uid@$host_address /etc/init.d/pdm start
+    ssh $uid@$host_address /etc/init.d/pdm start &
 }
 
 function deploy()
 {
-    echo "shutting down http://$host_address:$host_port"
+    echo "shutting down..."
     ssh $uid@$host_address /etc/init.d/pdm stop
     echo "deploying package to server"
     scp $jar_home/target/easyassess-pdm-0.0.1.jar $uid@$host_address:$api_service_path
