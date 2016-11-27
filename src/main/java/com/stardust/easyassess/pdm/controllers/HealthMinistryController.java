fe8bc8ba587e8d9d7dba1f9e9bc9c12b2b2ec712
Dong Chen <dongchen@thoughtworks.com>
2016-11-27 22:58:54 +0800

    add the api for modify self ministry

--- src/main/java/com/stardust/easyassess/pdm/controllers/HealthMinistryController.java
+++ src/main/java/com/stardust/easyassess/pdm/controllers/HealthMinistryController.java
@@ -1,5 +1,7 @@
 package com.stardust.easyassess.pdm.controllers;
 
+import com.stardust.easyassess.core.exception.ESAppException;
+import com.stardust.easyassess.core.presentation.ViewJSONWrapper;
 import com.stardust.easyassess.pdm.common.ListSelectionBuilder;
 import com.stardust.easyassess.core.query.Selection;
 import com.stardust.easyassess.pdm.common.ViewContext;
@@ -7,13 +9,35 @@ import com.stardust.easyassess.pdm.models.HealthMinistry;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping({"{domain}/data/ministry"})
 @EnableAutoConfiguration
-public class HealthMinistryController extends AbstractMaintenanceController<HealthMinistry>  {
+public class HealthMinistryController extends AbstractMaintenanceController<HealthMinistry> {
+
+    @ResponseBody
+    @RequestMapping(value = "/myministry", method = {RequestMethod.PUT})
+    public ViewJSONWrapper updateSelfProfile(@RequestBody HealthMinistry model) throws ESAppException {
+        Map<String, Object> profile = getUserProfile();
+        if (profile != null) {
+            List<Long> ministries = (List<Long>) profile.get("ministries");
+            if (ministries.contains(model.getId())) {
+                if (preUpdate(model.getId(), model)) {
+                    return postUpdate(getService().save(model));
+                } else {
+                    return createEmptyResult();
+                }
+            } else throw new ESAppException("Permission Denied", 403);
+        } else {
+            return createEmptyResult();
+        }
+
+    }
+
+
     @Override
     protected String getModelName() {
         return "healthMinistry";
@@ -33,8 +57,8 @@ public class HealthMinistryController extends AbstractMaintenanceController<Heal
         listSelectionBuilders.put("affiliated", new ListSelectionBuilder() {
             @Override
             public List<Selection> buildSelections(List<Selection> selections, ViewContext context) {
-                List<Long> ministries = (List<Long>)getUserProfile().get("ministries");
-                Map<Long, String> ministryMap = (Map<Long, String>)getUserProfile().get("ministryMap");
+                List<Long> ministries = (List<Long>) getUserProfile().get("ministries");
+                Map<Long, String> ministryMap = (Map<Long, String>) getUserProfile().get("ministryMap");
                 if (ministries != null && ministries.size() > 0 && ministryMap != null) {
                     selections.add(new Selection("id", Selection.Operator.EQUAL, ministries.get(0)));
                 }
