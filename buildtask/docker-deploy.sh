1232b9b1f5ffa090fcd44a384598eb005b78bb23
Li Cheng <chengli@thoughtworks.com>
2017-01-29 10:50:14 +0800

    Dockerize service

---
+++ buildtask/docker-deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ENV_VARS="[\"ENV\":\"ES_ENV=prod\", \"ES_AUTHENTICATION_SERVER\":\"${PROD_AUTH_SERVER}\",\"ES_AUTHENTICATION_PORT\":\"1337\",\"ES_SESSION_SERVER\":\"${PROD_SESSION_SERVER}\",\"ES_SESSION_PORT\":\"6379\",\"ES_DB_SERVER\":\"${PROD_DB_SERVER}\",\"ES_DB_PORT\":\"3306\", \"ES_DB_USER\":"${PROD_DB_USER}",\"ES_DB_PASSWORD\":\"${PROD_DB_PASSWORD}\"]"
+DOCKER_IMAGE="registry-vpc.cn-beijing.aliyuncs.com/easyassess/pdm-service"
+HOST_CONFIG="{\"PortBindings\":{\"8180/tcp\": [{ \"HostPort\": \"8180\"}]}}"
+CONTAINER_CONFIG="{\"Name\": \"pdm-service\",\"Image\": \"$DOCKER_IMAGE\", \"Env\":$ENV_VARS, \"ExposedPorts\": {\"8180/tcp\": {}}, \"HostConfig\": $HOST_CONFIG}"
+#pull image
+curl -v -X POST "http://$TARGET_SERVER:2376/images/create?fromImage=$DOCKER_IMAGE"
+curl -v -H "Content-type: application/json" -X DELETE "http://$TARGET_SERVER:2376/containers/pdm-service?force=true"
+curl -v -H "Content-type: application/json" -X POST -d "$CONTAINER_CONFIG" "http://$TARGET_SERVER:2376/containers/create?name=pdm-service"
+curl -v -H "Content-type: application/json" -X POST "http://$TARGET_SERVER:2376/containers/pdm-service/start"
---
+++ buildtask/docker-push.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker tag registry.cn-beijing.aliyuncs.com/easyassess/pdm-service registry.cn-beijing.aliyuncs.com/easyassess/pdm-service:latest
+docker push registry.cn-beijing.aliyuncs.com/easyassess/pdm-service:latest
