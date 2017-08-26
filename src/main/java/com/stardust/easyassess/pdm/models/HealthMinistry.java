89f09ef6c9ef95aed13a440b236b9be819281470
Li Cheng <chengli@thoughtworks.com>
2017-08-26 12:17:16 +0800

    Ministry level and category

--- src/main/java/com/stardust/easyassess/pdm/models/HealthMinistry.java
+++ src/main/java/com/stardust/easyassess/pdm/models/HealthMinistry.java
@@ -33,6 +33,26 @@ public class HealthMinistry extends DataModel {
 
     private String logo;
 
+    private String level;
+
+    private String category;
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     public String getProvince() {
         return province;
     }
